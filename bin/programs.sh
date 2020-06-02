7714eef72990059b0734224ac58f4979e4ce459d
Diptesh Basak <bdiptesh@gmail.com>
2020-06-02 19:18:17 +0530

    v0.3.0
    Added bash script for python dependencies

---
+++ bin/programs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# =============================================================================
+# Python dependencies
+#
+# Objective: Install python dependencies from requirements.txt
+# 
+# Version: 0.2.0
+#
+# Author: Diptesh.Basak
+#
+# Date: Jun 02, 2020
+#
+# =============================================================================
+
+# Set project directory
+path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)\
+/$(basename "${BASH_SOURCE[0]}")"
+
+proj_dir=$(sed -E 's/(.+)(\/bin\/.+)/\1/' <<< $path)
+
+pip install -r $proj_dir/requirements.txt
