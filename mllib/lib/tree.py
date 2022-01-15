39cc1d5a7687d74a499b5a7332d15fafffe85b82
MadhuTangudu <madhu.tangudu@gmail.com>
2022-01-15 19:04:03 +0530

    v0.4.0
    changelog:
    
    - made changes to tree.py module as suggested by Diptesh (integration tests are pending)

--- mllib/lib/tree.py
+++ mllib/lib/tree.py
@@ -55,9 +55,8 @@ class Tree():
                  x_var: List[str] = None,
                  method: str = "regression",
                  k_fold: int = 5,
-                 max_lag: int = 20,
-                 threshold: float = 0.05,
-                 param: Dict = None):
+                 param: Dict = None,
+                 ts_params: Dict = None):
         """Initialize variables."""
         self.y_var = y_var
         self.x_var = x_var
@@ -66,13 +65,15 @@ class Tree():
         if self.method in ("classify", "regression"):
             self.df = self.df.reset_index(drop=True)
         self.k_fold = k_fold
-        self.max_lag = max_lag
-        self.threshold = threshold
         self.seed = 1
-        # TODO: Use a Dict to store all time series related values
         if self.method == "timeseries":
-            self.ts_x_var = None
-            self.ts_lag_var = None
+            self.ts_params = ts_params
+            if self.ts_params is None:
+                self.ts_params = {}
+                self.ts_params["threshold"] = 0.05
+                self.ts_params["max_lag"] = 20
+            self.ts_params["ts_x_var"] = None
+            self.ts_params["ts_lag_var"] = None
             self._ts_data_transform()
             self.k_fold = ts_split(n_splits=self.k_fold)\
                 .split(X=self.ts_df[self.y_var])
@@ -89,7 +90,8 @@ class Tree():
             y_hat = list(self.model.predict(self.df[self.x_var]))
         elif self.method == "timeseries":
             y = self.ts_df.loc[:, self.y_var].values.tolist()
-            y_hat = list(self.model.predict(self.ts_df[self.ts_x_var]))
+            y_hat = list(self.model.predict(
+                self.ts_df[self.ts_params["ts_x_var"]]))
         if self.method in ("regression", "timeseries"):
             model_summary = {"rsq": np.round(metrics.rsq(y, y_hat), 3),
                              "mae": np.round(metrics.mae(y, y_hat), 3),
@@ -118,32 +120,36 @@ class Tree():
         # Determine significant lags
         df = self.df.copy(deep=True)
         df = df[self.y_var]
-        df = pd.DataFrame({"lag": list(range(self.max_lag+1)),
-                           "pacf": pacf(df, nlags=self.max_lag, method='ols')})
-        df["thres_val"] = (np.round(norm.ppf(1 - (self.threshold / 2)), 2)
-                           / (len(self.df) ** 0.5))
+        df = pd.DataFrame({"lag": list(range(self.ts_params["max_lag"]+1)),
+                           "pacf": pacf(df,
+                                        nlags=self.ts_params["max_lag"],
+                                        method='ols')})
+        df["thres_val"] = \
+            (np.round(norm.ppf(1 - (self.ts_params["threshold"] / 2)), 2)
+             / (len(self.df) ** 0.5))
         df["pacf_sig"] = np.where((df['pacf'] >= df["thres_val"])
                                   | (df['pacf'] <= - df["thres_val"]),
                                   1, 0)
         df = df.where(df['pacf_sig'] > 0)
         df = df.dropna()
-        self.ts_lag_var = df['lag'].astype(int).to_list()
-        self.ts_lag_var.append(m)
-        for i in self.ts_lag_var:
-            if i == 0:
-                self.ts_lag_var.remove(i)
+        self.ts_params["ts_lag_var"] = df['lag'].astype(int).to_list()
+        self.ts_params["ts_lag_var"].append(m)
+        self.ts_params["ts_lag_var"]  = \
+            [x for x in self.ts_params["ts_lag_var"] if x != 0]
+        self.ts_params["ts_lag_var"] = set(self.ts_params["ts_lag_var"])
+        self.ts_params["ts_lag_var"] = list(self.ts_params["ts_lag_var"])
         self.ts_df = pd.DataFrame(self.df.loc[:, self.y_var])
         # TODO: Add integration test
-        if len(self.ts_lag_var) == 0:  # pragma: no cover
-            self.ts_lag_var = [1]
-        for lag in self.ts_lag_var:
-            self.ts_df.loc[:, "lag_" + str(lag)] = self.ts_df["y"].shift(lag)
+        if len(self.ts_params["ts_lag_var"]) == 0:  # pragma: no cover
+            self.ts_params["ts_lag_var"] = [1]
+        for lag in self.ts_params["ts_lag_var"]:
+            self.ts_df.loc[:, "lag_" + str(lag)] = \
+                                    self.ts_df[self.y_var].shift(lag)
         if self.x_var is not None:
             self.ts_df = self.ts_df.join(self.df[self.x_var])
         self.ts_df = self.ts_df.dropna()
-        self.ts_x_var = list(self.ts_df.columns)
-        # TODO: Check for consistency and edge cases
-        self.ts_x_var.remove(self.y_var[0])
+        self.ts_params["ts_x_var"] = list(self.ts_df.columns)
+        self.ts_params["ts_x_var"].remove(self.y_var)
 
     def _fit(self) -> Dict[str, Any]:  # pragma: no cover
         """Fit model."""
@@ -161,15 +167,14 @@ class Tree():
             df_op = x_predict.copy(deep=True)
             df_op[self.y_var] = -1.0
         lst_lag_val = self.df[self.y_var].tolist()
-        # TODO: Use enumerate
-        for i in range(0, len(df_op)):
+        for i, _ in enumerate(df_op):
             df_pred_x = pd.DataFrame(df_op.iloc[i]).T
-            # TODO: Use enumerate
-            for j in range(0, len(self.ts_lag_var)):
-                df_pred_x["lag_" + str(self.ts_lag_var[j])] \
-                    = lst_lag_val[len(lst_lag_val) - self.ts_lag_var[j]]
+            for j, _ in enumerate(self.ts_params["ts_lag_var"]):
+                df_pred_x["lag_" + str(self.ts_params["ts_lag_var"][j])] \
+                    = lst_lag_val[len(lst_lag_val) \
+                                  - self.ts_params["ts_lag_var"][j]]
             df_pred_x = pd.DataFrame(df_pred_x)
-            y_hat = self.model.predict(df_pred_x[self.ts_x_var])
+            y_hat = self.model.predict(df_pred_x[self.ts_params["ts_x_var"]])
             df_op.iloc[i, df_op.columns.get_loc(self.y_var)] = y_hat[0]
             lst_lag_val.append(y_hat[0])
         return df_op
@@ -281,9 +286,10 @@ class RandomForest(Tree):
                     = list(range(1, len(self.x_var)))
             elif self.method == "timeseries":
                 self.param["max_features"] \
-                    = [int(np.ceil(len(self.ts_x_var) / 3))]
+                    = [int(np.ceil(len(self.ts_params["ts_x_var"]) / 3))]
                 self.param["min_samples_leaf"] = [5]
-                self.param["max_depth"] = list(range(1, len(self.ts_x_var)))
+                self.param["max_depth"] = \
+                    list(range(1, len(self.ts_params["ts_x_var"])))
         if self.method == "classify":
             tmp_model = rf.RandomForestClassifier(oob_score=True,
                                                   random_state=self.seed)
@@ -298,9 +304,8 @@ class RandomForest(Tree):
                                 n_iter=3,
                                 return_train_score=True,
                                 cv=self.k_fold)
-        # TODO: Use a Dict to store all time series related values
         if self.method == "timeseries":
-            gs_op = gs.fit(self.ts_df[self.ts_x_var],
+            gs_op = gs.fit(self.ts_df[self.ts_params["ts_x_var"]],
                            self.ts_df[self.y_var])
         elif self.method in ("regression", "classify"):
             gs_op = gs.fit(self.df[self.x_var],
@@ -397,7 +402,8 @@ class XGBoost(Tree):
                           "colsample_bytree": [0.5, 1.0],
                           "min_child_weight": [0.5, 1.0, 3.0]}
             if self.method == "timeseries":
-                self.param["max_depth"] = [int(len(self.ts_x_var) * 0.8)]
+                self.param["max_depth"] = \
+                    [int(len(self.ts_params["ts_x_var"]) * 0.8)]
             elif self.method in ("regression", "classify"):
                 self.param["max_depth"] = [int(len(self.x_var) * 0.8)]
             if self.method == "classify":
@@ -427,7 +433,7 @@ class XGBoost(Tree):
                                 cv=self.k_fold,
                                 random_state=self.seed)
         if self.method == "timeseries":
-            gs_op = gs.fit(self.ts_df[self.ts_x_var],
+            gs_op = gs.fit(self.ts_df[self.ts_params["ts_x_var"]],
                            self.ts_df[self.y_var])
         elif self.method in ("regression", "classify"):
             gs_op = gs.fit(self.df[self.x_var],
